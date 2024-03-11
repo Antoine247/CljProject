abb2fbe8c11d16382fa4ffa602a6975e34957af5
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-03-11 14:30:15 -0300

    Namespace para eventos

---
+++ src/antoine/eventos.clj
@@ -0,0 +1,24 @@
+(ns antoine.eventos
+  (:require [antoine.sql.paciente_ambulatorio :as paciente]
+            [antoine.sql.seguridad_quirurgica :as seguridad]
+            [antoine.sql.alertas_asistenciales :as alerta]
+            [antoine.sql.his_lectora :as lectora]
+            [antoine.sql.anestesia_ambulatoria :as anes-ambu]))
+
+(defn reinicio-paciente
+  "llama a todas las funciones necesarias para borrar todos los registros de un paciente y poder usarlo desde 0
+   puedes llamar la funcion sola y te va a usar un paciente random o puedes darle la historia clinica"
+  [histcli] (let [paciente (paciente/limpiar-paciente (paciente/paciente-ambulatorio-aleatorio histcli) {:Guar_Estado 1,
+                                                                                               :Guar_Estado1 1,
+                                                                                               :Guar_Estado3 1
+                                                                                               :Guar_Medico 0,
+                                                                                               :Guar_TipoMed 0,
+                                                                                               :Guar_FechaAlta 0,
+                                                                                               :Guar_HoraAlta 0,
+                                                                                               :Guar_EspMed 0,
+                                                                                               :Guar_HoraAtenc 0})]
+              (seguridad/borrar paciente)
+              (alerta/borrar paciente)
+              (lectora/borrar paciente 9999)
+              (anes-ambu/borrar paciente)
+              paciente))
--- src/antoine/scriptQuirurgico.clj
+++ src/antoine/scriptQuirurgico.clj
@@ -2,12 +2,7 @@
   (:require [antoine.system :refer [configuracion]] 
             [clojure.java.io :as io]
             [antoine.servicios.conexiones :as conn]
-            [honey.sql :as sql]
-            [antoine.sql.paciente_ambulatorio :as pac]
-            [antoine.sql.seguridad_quirurgica :as sqa]
-            [antoine.sql.alertas_asistenciales :as ala]
-            [antoine.sql.his_lectora :as hl]
-            [antoine.sql.anestesia_ambulatoria :as anes-ambu]
+            [honey.sql :as sql] 
             [java-time.api :as jt]) 
   (:gen-class))
 
@@ -22,23 +17,6 @@
   [& args]
   (greet {:name (first args)}))
 
-(defn reinicio-paciente
-  "llama a todas las funciones necesarias para borrar todos los registros de un paciente y poder usarlo desde 0
-   puedes llamar la funcion sola y te va a usar un paciente random o puedes darle la historia clinica"
-  [histcli] (let [paciente (pac/limpiar-paciente (pac/paciente-ambulatorio-aleatorio histcli) {:Guar_Estado 1,
-                                                                                               :Guar_Estado1 1,
-                                                                                               :Guar_Estado3 1
-                                                                                               :Guar_Medico 0,
-                                                                                               :Guar_TipoMed 0,
-                                                                                               :Guar_FechaAlta 0,
-                                                                                               :Guar_HoraAlta 0,
-                                                                                               :Guar_EspMed 0,
-                                                                                               :Guar_HoraAtenc 0})]
-              (sqa/borrar paciente)
-              (ala/borrar paciente)
-              (hl/borrar paciente 9999)
-              (anes-ambu/borrar paciente)
-              paciente))
 
 (comment
   (sqa/borrar (pac/paciente-ambulatorio-aleatorio 758036))
